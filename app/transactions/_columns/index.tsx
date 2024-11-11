4f34d08e59f7d5bd637b355b8fdf45a111464983
HulitosCode <heltonmatimbe8@gmail.com>
2024-11-11 11:39:27 +0200

    feat: ensure transaction table labels are rendered properly

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -3,6 +3,31 @@
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
+import { Button } from "@/app/_components/ui/button";
+import { PencilIcon, TrashIcon } from "lucide-react";
+
+export const TRANSACTION_CATEGORY_LABLES = {
+  EDUCATION: "Educacao",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Alimentacao",
+  HEALTH: "Saude",
+  HOUSING: "Moradia",
+  OTHER: "Outros",
+  SALARY: "Salario",
+  TRANSPORTATION: "Transport",
+  UTTLITY: "Utilidades",
+};
+
+export const TRANSACTION_PAYMENT_METHOD_LABELS = {
+  BANK_TRANSFER: "Transferencia Bancaria",
+  BANK_SLIP: "Boleto Bancario",
+  CASH: "Dinheiro",
+  CREDIT_CARD: "Cartao de Credito",
+  DEBIT_CARD: "Cartao de Debito",
+  MPESA: "Carteira Movel M-pesa",
+  EMOLA: "Carteira Movel E-mola",
+  OTHER: "Outros",
+};
 
 export const transactioncolumns: ColumnDef<Transaction>[] = [
   {
@@ -19,21 +44,48 @@ export const transactioncolumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABLES[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Metodo de Pagamento",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "MZN",
+      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
-    header: "",
+    header: "Acoes",
+    cell: () => {
+      return (
+        <div className="space-x-1">
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <PencilIcon />
+          </Button>
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <TrashIcon />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,6 +2,7 @@ import { db } from "../_lib/prisma";
 import { Button } from "../_components/ui/button";
 import { DataTable } from "../_components/ui/data-table";
 import { transactioncolumns } from "./_columns";
+import { ArrowDownUpIcon } from "lucide-react";
 
 const TransactionPage = async () => {
   // acessar as transacoes do meu banco de dados
@@ -10,8 +11,11 @@ const TransactionPage = async () => {
     <div className="space-y-6 p-6">
       {/* TITULO E BOTAO*/}
       <div className="flex w-full items-center justify-between">
-        <h1>Transacoes</h1>
-        <Button>Adicionar transacao</Button>
+        <h1 className="text-2xl font-bold">Transacoes</h1>
+        <Button className="rounded-full font-bold">
+          Adicionar transacao
+          <ArrowDownUpIcon />
+        </Button>
       </div>
       <DataTable columns={transactioncolumns} data={transaction} />
     </div>
