d62703eb54594472608582d83437f05abf1e3fc7
HulitosCode <heltonmatimbe8@gmail.com>
2024-11-11 10:42:20 +0200

    feat: add transaction type badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,21 @@
-const TransictionPage = () => {
-    return (
-        
-    )
-}
+import { db } from "../_lib/prisma";
+import { Button } from "../_components/ui/button";
+import { DataTable } from "../_components/ui/data-table";
+import { transactioncolumns } from "./_columns";
 
-export default TransictionPage
+const TransactionPage = async () => {
+  // acessar as transacoes do meu banco de dados
+  const transaction = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* TITULO E BOTAO*/}
+      <div className="flex w-full items-center justify-between">
+        <h1>Transacoes</h1>
+        <Button>Adicionar transacao</Button>
+      </div>
+      <DataTable columns={transactioncolumns} data={transaction} />
+    </div>
+  );
+};
+
+export default TransactionPage;
